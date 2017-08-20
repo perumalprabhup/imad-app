284055011d257b35b53fd39770cb90125d30cb9c
prabhu <ppprabhu2010@gmail.com>
2017-08-20 11:01:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get('/', function (req, res) {
 app.get('/page2', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'firstPage.html'));
 });
+
 app.get('/page3', function (req, res) {
  res.send("Hi welcome to the 3nd page");
 });
