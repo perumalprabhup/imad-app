750d1d6072c5c1dadc518dd713fecea1ca757d5a
prabhu <ppprabhu2010@gmail.com>
2017-08-22 05:49:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,58 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var article_One={
+ title : "prabhu",
+ heading :'This is My First Page',
+ paragraph :`
+This is the Content Page what to say is nothing now it will be Continued.... 
+This is the advanced form of new datas and new Files 
+`};
+
+function createTemplate(data){
+    
+    var title=data.title;
+    var heading = data.heading;
+    var paragraph=date.paragraph;
+var htmlTemplate =`
+<html>
+<head>
+<title>${title}</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+ <link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container">
+        <div class=""> 
+        <a href="/">Home</a>
+        </div>
+<div class="row">
+    <div class="header">
+<h1>${heading}</h1>
+</div>
+
+<div class="body">
+<p>${paragraph}</p>
+</div>
+<div class="footer pull-right">
+<h6>Copyrights@given to Prabhu for this year 2016-2020</h6>
+</div>
+</div>
+</div>
+</body>
+</html>
+`;
+return htmlTemplate;
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/page1', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(article_one));
 });
 
 app.get('/page2', function (req, res) {
