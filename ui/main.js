937a00d830bc4807bb467e92922658df04f71ec1
prabhu <ppprabhu2010@gmail.com>
2017-08-23 22:21:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -79,7 +79,7 @@ var name = nameInput.value;
         
 //make the req
 
-request.open('GET','http://perumalprabhu92.imad.hasura-app.io/input_name?names='+name,true);
+request.open('GET','http://perumalprabhu92.imad.hasura-app.io/input_name?name='+name,true);
 request.send(null);
    
    
