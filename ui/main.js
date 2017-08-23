ad4b4b07659b83e41c421daeb2d83d897d156468
prabhu <ppprabhu2010@gmail.com>
2017-08-23 21:12:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -61,7 +61,7 @@ submitbtn.onclick = function(){
        
        var list = '';
     
-    for(i=0;i<names.length;i++){
+    for(var i=0;i<names.length;i++){
         
         list += '<li>'+names[i]+'</li>';
     }
