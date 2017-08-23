aeb920461ec994a2b60849eaf7eb93f15cdfe298
prabhu <ppprabhu2010@gmail.com>
2017-08-23 06:29:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,39 +1,12 @@
-console.log('Loaded!');
-//change the body conten
-/*
-var pp=document.getElementById("body-text");
 
-pp.innerHTML="<h1>HI how is this</h1>";
-//slide the image 
-
-
-var ppp=document.getElementById("img");
-var marginLeft=0;
-function moveRight(){
-    
-    marginLeft = marginLeft + '5';
-    img.style.marginLeft = marginLeft +'px';
-    
-    
-}
-ppp.onclick =function(){
-    
-    var interaval=setInterval(moveRight,5);
-    
-    //`ppp.style.marginLeft="50px";
-};
-
-
-*/
-
-var button =document.getElementById("counter");
+var button =document.getElementById('counter');
 
 var counter=0;
 button.onclick = function(){
     
-    counter=counter+1;
+    counter = counter + 1;
     
-    var span = document.getElementById("count");
+    var span = document.getElementById('count');
     
     span.innerHTML=counter.toString();
     
