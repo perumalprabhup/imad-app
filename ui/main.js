d2f268f95e3b8ae88f006fe364edaa7291e66a0f
prabhu <ppprabhu2010@gmail.com>
2017-08-23 07:11:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,35 @@
 
 var button = document.getElementById('counter');
 
-var counter = 0 ;    
-    
 button.onclick = function(){
     
+    //create a req object;
+    var req=new XMLHttpRequest();
     
+    //capture the response and store it in a variable
     
+    req.onreadystatechange =function(){
+        
+    if(req.readyState == XMLHttpRequest.DONE){
+        
+        if(req.status===200){
+            
+            var counter =req.responceText;
+                
+                var span = document.getElementById('count');
+    
+                span.innerHTML = counter.toString();
+    
+            
+        }
+    }
+        
+        
+    };
+    
+//make the req
 
-    //render the var in to the Correct span
-    
-    counter = counter + 1;
-    
-    var span = document.getElementById('count');
-    
-    span.innerHTML = counter.toString();
-    
+req.open('GET','http://perumalprabhu92.imad.hasura-app.io/counter',true);
+req.close('null');
     
 };
