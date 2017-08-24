3c1873f8268e56fa218c5fc6c6bb37b322b865d2
prabhu <ppprabhu2010@gmail.com>
2017-08-24 17:23:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,38 +1,4 @@
 
-var button = document.getElementById('counter');
-
-button.onclick = function(){
-    
-    //create a req object;
-    var request=new XMLHttpRequest();
-    
-    //capture the response and store it in a variable
-    
-    request.onreadystatechange =function(){
-        
-    if(request.readyState  == XMLHttpRequest.DONE){
-        
-        if(request.status === 200){
-            
-            var counter =request.responseText;
-                
-                var span = document.getElementById('count');
-                
-                span.innerHTML = counter .toString();
-    
-        }
-    }
-        
-        
-    };
-    
-//make the req
-
-request.open('GET','http://perumalprabhu92.imad.hasura-app.io/counter',true);
-request.send(null);
-    
-};
-
 
 var submitbtn = document.getElementById('submit_btn');
 
@@ -84,5 +50,43 @@ request.send(null);
    
    
 };
+
+
+
+
+var button = document.getElementById('counter');
+
+button.onclick = function(){
+    
+    //create a req object;
+    var request=new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+    
+    request.onreadystatechange =function(){
+        
+    if(request.readyState  == XMLHttpRequest.DONE){
+        
+        if(request.status === 200){
+            
+            var counter =request.responseText;
+                
+                var span = document.getElementById('count');
+                
+                span.innerHTML = counter .toString();
+    
+        }
+    }
+        
+        
+    };
+    
+//make the req
+
+request.open('GET','http://perumalprabhu92.imad.hasura-app.io/counter',true);
+request.send(null);
+    
+};
+
 
 
