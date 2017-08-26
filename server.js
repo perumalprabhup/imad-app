99273d8d3d00560062346e7ae6219c5f77421319
prabhu <ppprabhu2010@gmail.com>
2017-08-26 08:44:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,9 +105,11 @@ app.get('/test',function(req,res){
     pool.query('SELECT * FROM test',function(err,result){
         
         if(err){
-            err.status(500).send(err,toString("prabhu Error"));
+            err.status(500).send(err,toString());
         }
-        
+        else{
+            res.send(JSON.stringify(result));
+        }
     });
     
     
