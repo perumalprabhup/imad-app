ce553aaa29ee5f09f6a282ca85e33ed70cb37dec
prabhu <ppprabhu2010@gmail.com>
2017-08-27 09:28:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.get('/articles/:articleName', function (req, res) {
          }
          else{
              
-                var articleData = result.rows[0];
+                var articleData = result.rows[1];
              
                 res.send(createTemplate(articleData));
          
