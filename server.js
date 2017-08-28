fef2ad43bb5feb408b63f3fbeb1295c4b684c3a6
prabhu <ppprabhu2010@gmail.com>
2017-08-28 17:59:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,6 +90,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+
+
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     
@@ -117,7 +120,7 @@ app.get('/articles/:articleName', function (req, res) {
      }
      else{
          if(result.rows.length === 0){
-             res.status(400).send("Article not found");
+             res.status(404).send("Article not found");
          }
          else{
              
