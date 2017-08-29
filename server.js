158763a78c90164d55f6a2d7db95ab8a9c84ff6c
prabhu <ppprabhu2010@gmail.com>
2017-08-29 19:07:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ function hash(input,salt){
     
     var hashed = crypto.pbkdf2Sync('input', 'salt', 100000, 512, 'sha512');
     
-    return hashed.toString(hex);
+    return hashed.toString('hex');
  
     
     
